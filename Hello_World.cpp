f790290008c1ace61f8b4abbb8e08fe5f8ce67a2
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-29 21:03:00 -0500

    We've just created a program that prints 'Hello World'

---
+++ Hello_World.cpp
@@ -0,0 +1,7 @@
+#include <iostream>	//IOSTREAM stands for input-ouput stream
+using namespace std;	//STD stands for STANDARD
+
+int main(){
+	cout<<"Hello World"<<endl;	//COUT allows to output some text
+	return 0;
+}
