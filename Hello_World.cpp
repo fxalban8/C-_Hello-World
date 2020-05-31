1cb2c80c46d6c243e18ce282f4541cded70e6ac9
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 19:36:52 -0500

    Adding the function cin.get() to wait for the user to press ENTER before ending the program

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,5 +3,9 @@ using namespace std;	//STD stands for STANDARD
 
 int main(){
 	cout<<"Hello World"<<endl;	//COUT allows to output some text
+
+	//waiting for the user to press ENTER
+        cin.get();
+        
 	return 0;
 }
